17c9446f5955b402b2d0b1f296c587ee8d738c60
Jethro Flanagan <jethroflanagan@gmail.com>
2022-02-22 14:22:25 +0200

    Delays running Segment, GTM, Dino, Cookie Banner

--- global/dinosaur-footer.js
+++ global/dinosaur-footer.js
@@ -1,15 +1,19 @@
-window.$loaded(function(window, document, $, undefined) {
-  $('.footer__dinosaur').on('click', function(event) {
-    $('.footer__dinosaur').removeClass('dino-run');
+!function(){
+  const dino = window.$loaded(function(window, document, $, undefined) {
+    $('.footer__dinosaur').on('click', function(event) {
+      $('.footer__dinosaur').removeClass('dino-run');
 
-    var dinoSvg1 = $('.dino-step-1');
-    var dinoSvg2 = $('.dino-step-2');
+      var dinoSvg1 = $('.dino-step-1');
+      var dinoSvg2 = $('.dino-step-2');
 
-    dinoSvg1.addClass('dino-step-1-animation');
-    dinoSvg2.addClass('dino-step-2-animation');
+      dinoSvg1.addClass('dino-step-1-animation');
+      dinoSvg2.addClass('dino-step-2-animation');
 
-    setTimeout(function () {
-      $('.footer__dinosaur').addClass('dino-run');
-    }, 100)
+      setTimeout(function () {
+        $('.footer__dinosaur').addClass('dino-run');
+      }, 100)
+    });
   });
-});
+
+  setTimeout(dino, 4000);
+}()
