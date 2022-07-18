ccfeb30e880e373ea3242306699659ba2bec085a
Markus Kuhn <mkk0856@gmail.com>
2022-07-18 15:44:37 +0200

    Review changes

--- global/community-activities@1.0.js
+++ global/community-activities@1.0.js
@@ -1,16 +1,17 @@
 !function(){
   window.$loaded(function (window, document, $, undefined) {
     var form = $(".events-form");
-    var originalButtonText = $(":input[type='submit']").val();
+    const submitButton = form.find(":input[type='submit']");
+    const originalButtonText = submitButton.val();
 
     var submissionSuccess = function(response) {
       form.submit();
-      $(":input[type='submit']").val(originalButtonText);
+      submitButton.val(originalButtonText);
     }
 
     var handleSubmissionFailure = function(response) {
       $(".w-form-fail").show();
-      $(":input[type='submit']").val(originalButtonText);
+      submitButton.val(originalButtonText);
     }
 
     var submitFormContent = function(e, token) {
@@ -19,10 +20,8 @@
       e.stopPropagation();
       e.stopImmediatePropagation();
 
-      var submitButton = $("#submit_button");
-
       // configured Growth Form values
-      var form_id = submitButton.attr('data-form-id');
+      var formId = submitButton.attr('data-form-id');
       var category = submitButton.attr("data-category");
       var action = submitButton.attr("data-action");
       var source = submitButton.attr("data-source");
@@ -31,34 +30,31 @@
 
       // Required form fields
       var email = document.querySelector("#email").value;
-      var first_name = document.querySelector("#first_name").value;
-      var last_name = document.querySelector("#last_name").value;
-
-      var subscribe_to_company_newsletter = document.querySelector("#subscribe_to_company_newsletter").checked;
-      var subscribe_to_hiring_insights = document.querySelector("#subscribe_to_company_newsletter").checked;
+      var firstName = document.querySelector("#first_name").value;
+      var lastName = document.querySelector("#last_name").value;
 
       // Extra fields which could be required per Webflow form
       var fields = ['company_name', 'job_title'];
       var emailSubscriptions = ['subscribe_to_company_newsletter', 'subscribe_to_blog', 'subscribe_to_hiring_insights', 'subscribe_to_events', 'subscribe_to_job_opportunities'];
 
       var payload = {
-        form_id,
+        form_id: formId,
         category,
         action,
         source,
         label,
         source_detail,
         email,
-        first_name,
-        last_name,
+        first_name: firstName,
+        last_name: lastName,
         "g-recaptcha-response-data": { "webflow": token }
       };
 
       // Loop through optional fields and append them to the payload
       for(i = 0; i < fields.length; i++) {
-        var textField = document.querySelector("#" + fields[i]);
+        var textField = document.querySelector(`input[name='${fields[i]}']`);// document.querySelector("#" + fields[i]);
 
-        if (textField && typeof textField !== 'undefined') {
+        if (textField != null) {
           payload[fields[i]] = textField.value;
         }
       }
@@ -77,17 +73,17 @@
         data: JSON.stringify(payload),
         contentType: 'application/json',
         type: 'POST',
-        url: 'https://9fdf-41-71-3-15.ngrok.io/api/growth/forms/community_activities',
+        url: '/api/growth/forms/community_activities',
         success: submissionSuccess,
         error: handleSubmissionFailure
       });
     }
 
-    $(document).on("click", ":input[type='submit']", function(e) {
+    $(document).on("click", submitButton[0], function(e) {
     	e.preventDefault();
       e.stopPropagation();
       e.stopImmediatePropagation();
-      $(":input[type='submit']").val("Submitting...");
+      submitButton.val("Submitting...");
 
       grecaptcha.ready(function() {
         grecaptcha.execute('6Lf802weAAAAAHgxndx9NIZ3FTzdG3f7nBua2rRY', { action: 'webflow' }).then(function(token) {
