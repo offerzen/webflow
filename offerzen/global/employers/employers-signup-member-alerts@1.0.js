bd9670d759c01d6d7d5ec9ec54864bb9c94d6da7
Diksha <dikshram@gmail.com>
2022-08-04 10:44:29 +0200

    handle member alerts on webflow

---
+++ offerzen/global/employers/employers-signup-member-alerts@1.0.js
@@ -0,0 +1,45 @@
+!(function () {
+  window.$loaded(function (window, document, $, undefined) {
+    const params = new URLSearchParams(window.location.search);
+    $('js-member-alert-message').val('');
+
+    if (params.has('member')) {
+      const alert = params.get('member');
+      //show the relevant message based on alert
+      switch (alert) {
+        case 'invalid_token':
+          $('.js-member-alert-message').text(
+            'This invite has been used already.'
+          );
+          $('.js-member-alert').show();
+          break;
+        case 'already_exists':
+          $('.js-member-alert-message').text(
+            "You've already been added as a member."
+          );
+          $('.js-member-alert').show();
+          break;
+        default:
+          return;
+      }
+
+      //clean url
+      var cleanSearch = window.location.search
+        .replace(/member[^&]+&?/g, '')
+        .replace(/&$/, '')
+        .replace(/^\?$/, '');
+
+      window.history.replaceState(
+        {},
+        '',
+        window.location.pathname + cleanSearch
+      );
+    }
+
+    //handle close alert icon
+    $('.js-close-alert').on('click', this, function (e) {
+      e.preventDefault();
+      $('.js-member-alert').hide();
+    });
+  });
+})();
