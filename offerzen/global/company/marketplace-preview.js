e7dfe5803cc381ff2ad97a2a365c415da2e695bd
Michael Kriel <michael616kriel@gmail.com>
2022-11-21 14:55:03 +0200

    Added filter limiting

--- offerzen/global/company/marketplace-preview.js
+++ offerzen/global/company/marketplace-preview.js
@@ -1,4 +1,4 @@
-$loaded(function() {
+$loaded(function () {
   let locationItems = $('.js-filter-location');
   let roleItems = $('.js-filter-role');
   let skillItems = $('.js-filter-skill');
@@ -8,35 +8,45 @@ $loaded(function() {
     skills: [],
   };
 
-  function onFilterToggle(elements, items) {
-    elements.on('click', function() {
+  function onFilterToggle(
+    elements,
+    items,
+    limit = null,
+    limitErrorSelector = null
+  ) {
+    elements.on('click', function () {
       const elementContent = $(this).text();
-      console.log(elementContent, items, items.includes(elementContent))
       if (items.includes(elementContent)) {
-        let index = items.findIndex(function(filterItem) {
+        let index = items.findIndex(function (filterItem) {
           return filterItem === elementContent;
         });
         if (index > -1) {
           items.splice(index, 1);
-          $(this).removeClass("js-filter-selected")
+          $(this).removeClass('js-filter-selected');
+          if (limit !== null && items.length < limit) {
+            $(limitErrorSelector).hide();
+          }
         }
       } else {
+        if (limit !== null && items.length >= limit) {
+          $(limitErrorSelector).show();
+          return;
+        }
         items.push(elementContent);
-        $(this).addClass("js-filter-selected")
+        $(this).addClass('js-filter-selected');
       }
     });
   }
 
-  function onSubmit() {
+  function onSubmit(event) {
     event.preventDefault();
     const params = new URLSearchParams(filters).toString();
-    console.log(filters)
-    window.location = $(this).attr('href') + "?" + params
+    window.location = $(this).attr('href') + '?' + params;
   }
 
   onFilterToggle(locationItems, filters.locations);
   onFilterToggle(roleItems, filters.roles);
-  onFilterToggle(skillItems, filters.skills);
+  onFilterToggle(skillItems, filters.skills, 3, '.js-filter-skill-error');
 
-  $('#submit-filters').on('click', onSubmit)
-})
+  $('#submit-filters').on('click', onSubmit);
+});
