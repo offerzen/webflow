5ea7007ccb7d72a02e5b307722cacf27a960077b
Jethro Flanagan <jethroflanagan@gmail.com>
2022-11-21 13:54:17 +0200

    Added $loaded

--- offerzen/global/company/marketplace-preview.js
+++ offerzen/global/company/marketplace-preview.js
@@ -1,40 +1,42 @@
-let locationItems = $('.js-filter-location');
-let roleItems = $('.js-filter-role');
-let skillItems = $('.js-filter-skill');
-let filters = {
-  locations: [],
-  roles: [],
-  skills: [],
-};
+$loaded(function() {
+  let locationItems = $('.js-filter-location');
+  let roleItems = $('.js-filter-role');
+  let skillItems = $('.js-filter-skill');
+  let filters = {
+    locations: [],
+    roles: [],
+    skills: [],
+  };
 
-function onFilterToggle(elements, items) {
-  elements.on('click', function() {
-    const elementContent = $(this).text();
-    console.log(elementContent, items, items.includes(elementContent))
-    if (items.includes(elementContent)) {
-      let index = items.findIndex(function(filterItem) {
-        return filterItem === elementContent;
-      });
-      if (index > -1) {
-        items.splice(index, 1);
-        $(this).removeClass("js-filter-selected")
+  function onFilterToggle(elements, items) {
+    elements.on('click', function() {
+      const elementContent = $(this).text();
+      console.log(elementContent, items, items.includes(elementContent))
+      if (items.includes(elementContent)) {
+        let index = items.findIndex(function(filterItem) {
+          return filterItem === elementContent;
+        });
+        if (index > -1) {
+          items.splice(index, 1);
+          $(this).removeClass("js-filter-selected")
+        }
+      } else {
+        items.push(elementContent);
+        $(this).addClass("js-filter-selected")
       }
-    } else {
-      items.push(elementContent);
-      $(this).addClass("js-filter-selected")
-    }
-  });
-}
+    });
+  }
 
-function onSubmit() {
-  event.preventDefault();
-  const params = new URLSearchParams(filters).toString();
-  console.log(filters)
-  window.location = $(this).attr('href') + "?" + params
-}
+  function onSubmit() {
+    event.preventDefault();
+    const params = new URLSearchParams(filters).toString();
+    console.log(filters)
+    window.location = $(this).attr('href') + "?" + params
+  }
 
-onFilterToggle(locationItems, filters.locations);
-onFilterToggle(roleItems, filters.roles);
-onFilterToggle(skillItems, filters.skills);
+  onFilterToggle(locationItems, filters.locations);
+  onFilterToggle(roleItems, filters.roles);
+  onFilterToggle(skillItems, filters.skills);
 
-$('#submit-filters').on('click', onSubmit)
+  $('#submit-filters').on('click', onSubmit)
+})
