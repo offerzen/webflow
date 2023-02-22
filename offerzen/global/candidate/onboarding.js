76a155a27bf8d976c931406d6a0333f28867a20f
Jethro Flanagan <jethroflanagan@gmail.com>
2023-02-22 15:41:08 +0200

    Check URL

--- offerzen/global/candidate/onboarding.js
+++ offerzen/global/candidate/onboarding.js
@@ -1,1 +1,8 @@
-!(function() { if (rx) rx.trigger('apply'); })()
+!(function() {
+  const cb = function(e) {
+    if (rx && window.location.includes('area-roles')) {
+      rx.trigger('apply');
+    }
+  }
+  window.addEventListener('popstate', cb, { once: true });
+})();
