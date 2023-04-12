1e99b51573b34683e06d421afe77d949c360b82b
Dominica Wannenburg <dominica@offerzen.com>
2023-04-12 12:05:44 +0200

    Updated error message

--- offerzen/global/company-fast-track-access/company-prospect-form.js
+++ offerzen/global/company-fast-track-access/company-prospect-form.js
@@ -107,7 +107,7 @@
               404: function () {
                 enableSubmitButton();
                 errorText.text(
-                  'Oops! There has been an error.'
+                  'Oops! Something went wrong while submitting the form.'
                 );
                 errorText.show();
                 clearTimeout(buttonLabelTimer);
